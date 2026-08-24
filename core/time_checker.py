2a02f425c63a19b79151ee4695999011f78669aa
EmmanSquared <emman30zb@gmail.com>
2026-08-24 21:09:28 +0800

    fixes

--- core/time_checker.py
+++ core/time_checker.py
@@ -7,6 +7,6 @@ class TimeChecker:
         self.chat_time = datetime.now().replace(hour=target_h,minute=target_m,second=target_s,microsecond=target_ms)
         self.timeleft_till_next_reminder = ((self.chat_time - datetime.now()).total_seconds()) - 30
 
-thirty_mins_before = TimeChecker(19,30,0,0)
+one_hour_before = TimeChecker(19,0,0,0)
 fifteen_mins_before = TimeChecker(19,45,0,0)
 meet_reminder = TimeChecker(20,0,0,0)
--- main.py
+++ main.py
@@ -1,23 +1,36 @@
 import core
 
 bool_node = True
+first_pass = True
 
 while bool_node:
-    if core.thirty_mins_before.chat_time > core.datetime.now():
-        print(f'Minutes left before reminding: {round(core.thirty_mins_before.timeleft_till_next_reminder/60)}.')
-        core.sleep(core.thirty_mins_before.timeleft_till_next_reminder)
+    if core.one_hour_before.chat_time > core.datetime.now():
+        print(f'{core.datetime.now()} Minutes left before reminding: {round(core.one_hour_before.timeleft_till_next_reminder/60)}.')
+        print('at 1 hour')
+        core.sleep(core.one_hour_before.timeleft_till_next_reminder)
+        if first_pass:
+            first_pass = False
+            continue
         core.window_tasks()
+        first_pass = True
 
     elif core.fifteen_mins_before.chat_time > core.datetime.now():
-        print(f'Minutes left before reminding: {round(core.fifteen_mins_before.timeleft_till_next_reminder/60)}.')
+        print(f'{core.datetime.now()} Minutes left before reminding: {round(core.fifteen_mins_before.timeleft_till_next_reminder/60)}.')
+        print('at 15 mins')
         core.sleep(core.fifteen_mins_before.timeleft_till_next_reminder)
+        if first_pass:
+            first_pass = False
+            continue
         core.window_tasks()
-        core.sleep(600)
-        core.window_tasks()
+        first_pass = True
 
-    elif core.meet_reminder.timeleft_till_next_reminder > core.datetime.now():
-        print(f'Minutes left before reminding: {round(core.fifteen_mins_before.timeleft_till_next_reminder/60)}.')
+    elif core.meet_reminder.chat_time > core.datetime.now():
+        print(f'{core.datetime.now()} Minutes left before reminding: {round(core.fifteen_mins_before.timeleft_till_next_reminder/60)}.')
+        print('at 0 mins')
         core.sleep(core.meet_reminder.timeleft_till_next_reminder)
+        if first_pass:
+            first_pass = False
+            continue
         core.window_tasks()
 
     else:
