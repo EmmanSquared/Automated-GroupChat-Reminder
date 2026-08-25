9a65ac6f67aa78c4c5141455c6e1f4346cda57ab
EmmanSquared <emman30zb@gmail.com>
2026-08-25 19:40:29 +0800

    dynamic time fix

--- core/time_checker.py
+++ core/time_checker.py
@@ -5,8 +5,12 @@ from datetime import datetime
 class TimeChecker:
     def __init__(self,target_h:int,target_m:int,target_s:int,target_ms:0):
         self.chat_time = datetime.now().replace(hour=target_h,minute=target_m,second=target_s,microsecond=target_ms)
-        self.timeleft_till_next_reminder = ((self.chat_time - datetime.now()).total_seconds()) - 30
+
+    def __getattr__(self,variable):
+        if variable == "timeleft_till_next_reminder":
+            return ((self.chat_time - datetime.now()).total_seconds()) - 30
 
 one_hour_before = TimeChecker(19,0,0,0)
 fifteen_mins_before = TimeChecker(19,45,0,0)
 meet_reminder = TimeChecker(20,0,0,0)
+
--- main.py
+++ main.py
@@ -1,36 +1,24 @@
 import core
 
 bool_node = True
-first_pass = True
 
 while bool_node:
     if core.one_hour_before.chat_time > core.datetime.now():
         print(f'{core.datetime.now()} Minutes left before reminding: {round(core.one_hour_before.timeleft_till_next_reminder/60)}.')
         print('at 1 hour')
         core.sleep(core.one_hour_before.timeleft_till_next_reminder)
-        if first_pass:
-            first_pass = False
-            continue
         core.window_tasks()
-        first_pass = True
 
     elif core.fifteen_mins_before.chat_time > core.datetime.now():
         print(f'{core.datetime.now()} Minutes left before reminding: {round(core.fifteen_mins_before.timeleft_till_next_reminder/60)}.')
         print('at 15 mins')
         core.sleep(core.fifteen_mins_before.timeleft_till_next_reminder)
-        if first_pass:
-            first_pass = False
-            continue
         core.window_tasks()
-        first_pass = True
 
     elif core.meet_reminder.chat_time > core.datetime.now():
         print(f'{core.datetime.now()} Minutes left before reminding: {round(core.fifteen_mins_before.timeleft_till_next_reminder/60)}.')
         print('at 0 mins')
         core.sleep(core.meet_reminder.timeleft_till_next_reminder)
-        if first_pass:
-            first_pass = False
-            continue
         core.window_tasks()
 
     else:
